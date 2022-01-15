209e24dd58803306e6cc8e954ded818c781b2360
Joanna <m.joanna.foss@gmail.com>
2022-01-15 00:33:02 -0600

    created a scraper

---
+++ src/main/resources/static/js/scrapers.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+async function scrapeProduct(url) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	const [el] = await page.$x('//*[@id="trails_table"]/tbody/tr/td[2]/a');
+	const href = await el.getProperty('href');
+	const hrefTxt = await href.jsonValue();
+
+	console.log({hrefTxt});
+
+	browser.close();
+}
+
+scrapeProduct('https://www.trailforks.com/search/?search=tongariro%20alpine%20crossing');
